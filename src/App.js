bb09107a50fdb08d7407cd426eefe61f75a050e2
YichaoW <wangyic@uw.edu>
2019-06-09 15:46:56 -0700

    titlestyle

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ import {SortingAlgorithms} from './Components/SortingAlgorithms'
 function App() {
   return (
     <div className="App">
+      <div id="title-box">
       <p id="main-title">Sorting Algorithm 101</p>
+      <p id="sub-title">Better than the Algorithm Class in College</p>
+      <p>By Tao Long, Haoran Pu, Yichao Wang</p>
+      </div>
       <Intro />
       <SortingAlgorithms/>
       <ComplexityAnalysis />
