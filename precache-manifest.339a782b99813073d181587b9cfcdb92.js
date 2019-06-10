5ac464ad23693cb612faa93ac93f93fec7d28fce
YichaoW <wangyic@uw.edu>
2019-06-09 17:12:30 -0700

    Updates

---
+++ precache-manifest.339a782b99813073d181587b9cfcdb92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa704a19508eddb11f7f030007561e7",
+    "url": "/info474final/index.html"
+  },
+  {
+    "revision": "0a582ec441a5787fe152",
+    "url": "/info474final/static/css/main.4a690c2c.chunk.css"
+  },
+  {
+    "revision": "925e537715b7389a980a",
+    "url": "/info474final/static/js/2.c8ce7b43.chunk.js"
+  },
+  {
+    "revision": "0a582ec441a5787fe152",
+    "url": "/info474final/static/js/main.3aa8cb07.chunk.js"
+  },
+  {
+    "revision": "3c7f14288bfbbe8edd04",
+    "url": "/info474final/static/js/runtime~main.fc257b73.js"
+  },
+  {
+    "revision": "29d7fe4d2ea440f4f8196907cfeede5a",
+    "url": "/info474final/static/media/spaceplot.29d7fe4d.png"
+  },
+  {
+    "revision": "1abd1fac433f15e214b9b8c2187babb8",
+    "url": "/info474final/static/media/timeplot.1abd1fac.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b659ba54daf5462a0408b1df8d50d69.js"
+  "/info474final/precache-manifest.339a782b99813073d181587b9cfcdb92.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/info474final/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
